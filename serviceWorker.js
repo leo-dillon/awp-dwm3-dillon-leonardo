54fb73d1235c19128bce831a90bc3d2fec2ae0d8
leo-dillon <jeannotegui@gmail.com>
2024-10-07 11:10:29 -0300

    Agregando servers worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,28 +1,84 @@
-let cacheName = 'leoCache'
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(cacheName)
-        .then(cache => {
-            cache.addAll(
-                [
-                'index.html'
-                ])
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceWorker.js')
+        .then(function (registration) {
         })
-    )
+        .catch(function (error) {
+        });
+}
 
-})
+// Nombre del caché
+var cacheName = 'rickAndMortyCache';
 
-self.addEventListener('activate', (e) => {
-    console.log('El SW esta activo', e);
-})
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request) 
-            .then( response => {
-                if( response ){
-                    return response
-                }
-                return fetch(e.request)
+// Archivos que queremos almacenar en caché
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/pag/favoritos.html',
+    '/estilos/index.css',
+    '/estilos/general.css',
+    '/IMG/logo.png',
+    '/IMG/rickMorty.png',
+    '/IMG/smoke.png',
+];
+
+// Evento de instalación
+self.addEventListener('install', function (event) {
+    // Esperar hasta que el caché esté abierto y los archivos hayan sido añadidos
+    event.waitUntil(
+        caches.open(cacheName)  // Abre el caché con el nombre dado
+            .then(function (cache) {
+                return cache.addAll(filesToCache);  // Añade todos los archivos en caché
             })
-    )
-})
+            .finally(()=>{
+                console.log(`cache almacenado ` )
+            })
+    );
+});
+
+// Evento de activación (usualmente usado para limpiar cachés antiguos)
+self.addEventListener('activate', function (event) {
+
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (thisCacheName) {
+                    if (thisCacheName !== cacheName) {
+                        return caches.delete(thisCacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Evento de fetch (intercepta las solicitudes de red)
+self.addEventListener('fetch', function (event) {
+
+    event.respondWith(
+        caches.match(event.request)  // Busca en el caché
+            .then(function (response) {
+                if(response?.status == 200){
+                    // Si el archivo está en caché, devuélvelo
+                    return response;
+
+                }                
+
+                // Si no está en caché, hacer la solicitud de red y luego almacenarlo en caché
+                    return fetch(event.request)
+                        .then(function (response) {
+                            return caches.open(cacheName)   
+                                .then(function (cache) {
+                                // Guardar una copia del archivo en el caché
+                                cache.put(event.request, response.clone());
+                                return response;  // Devolver el recurso solicitado
+                            }
+                                .catch(err)
+                            );
+                        }
+                        )
+            })
+            .catch(err =>  {
+                console.error("Error", err)
+            })
+    );
+});
